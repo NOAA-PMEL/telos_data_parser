c8605031ac306481ea9a2765e93331c7e184ec31
shjewellEDD <scott.jewell@noaa.gov>
2022-07-29 16:15:12 -0700

    Added GUI. Mostly works. Few things to fix before it's ready.

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import csv
 import re
 from dateutil import parser
 from datetime import timedelta
+import dearpygui.dearpygui as dpg
 import os
 
 import_path = f'D:\Data\TELOS Parsing\\07262022.TXT'
 export_path = f'D:\Data\TELOS Parsing'
+
+'''
+TODO:
+    Check import and export things
+    Creat some sort of text log output for errors and stats.
+'''
 
 def read_and_parse(path):
 
@@ -89,9 +96,73 @@ def write_files_and_folders(path, data, day):
             for line in data[inst]:
                 wf.write(line)
 
+def GUI():
+
+    dpg.create_context()
+
+    def check_export():
+
+        if os.path.isfile(dpg.get_value('input_file')):
+
+            data, date, errors = read_and_parse(import_path)
+            write_files_and_folders(export_path, data, date)
+
+            dpg.set_value('report', f'Exported. \n {errors}')
+
+        else:
+
+            dpg.set_value('report', 'Set input filepath!')
+
+
+    def get_file_path():
+        file_uid = dpg.generate_uuid()
+
+        with dpg.file_dialog(label='File Dialog', width=500, height=400, show=False, tag=file_uid,
+                             default_path=os.path.abspath(os.path.curdir),
+                             callback=lambda s, a, u: dpg.set_value('input_file', list(a['selections'].values())[0])):
+            dpg.add_file_extension('.*')
+            dpg.add_file_extension('')
+            dpg.add_file_extension('.txt')
+
+        dpg.show_item(file_uid)
+
+    def get_folder_path():
+        folder_uid = dpg.generate_uuid()
+
+        with dpg.file_dialog(label='Folder Dialog', width=500, height=400, show=False, tag=folder_uid,
+                             default_path=os.path.abspath(os.path.curdir), directory_selector=True,
+                             callback=lambda s, a, u: dpg.set_value('export_path', a['file_path_name'])):
+            dpg.add_file_extension('.*')
+            dpg.add_file_extension('')
+            dpg.add_file_extension('.txt')
+
+        dpg.show_item(folder_uid)
+
+    with dpg.window(tag='primary'):
+
+        with dpg.group(horizontal=True):
+            dpg.add_input_text(tag='input_file', label='', width=400)
+            dpg.add_button(tag='get_import_path', label='Open', callback=get_file_path)
+
+        with dpg.group(horizontal=True):
+            dpg.add_input_text(tag='export_path', default_value=os.path.abspath(os.path.curdir), width=400)
+            dpg.add_button(tag='get_export_path', label='Open', callback=get_folder_path)
+
+        dpg.add_button(tag='export', label='Parse and Export', callback=check_export)
+
+        dpg.add_text(tag='report')
+        dpg.add_
+
+    dpg.create_viewport(title='TELOS Data Parsing', width=600, height=600)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window('primary', True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
 
 if __name__ == '__main__':
 
-    data, date, errors = read_and_parse(import_path)
-    print(errors)
-    write_files_and_folders(export_path, data, date)
+    GUI()
+    # data, date, errors = read_and_parse(import_path)
+    # print(errors)
+    # write_files_and_folders(export_path, data, date)
